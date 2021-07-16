e1aaec931c8dbfeb539a0963cd6527c807fc119a
UselessAqua <UselessAquaticKat@gmail.com>
2021-07-17 00:22:13 +0400

    Теперь можно видеть список блогов на главной странице и список статей внутри блогов

--- smth/blogs/views.py
+++ smth/blogs/views.py
@@ -3,17 +3,25 @@ from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
 
-from .models import Blog
-
+from .models import Blog, Recording
 
 def index(request):
-    return HttpResponse("Main page")
+    latest_blogs_list = Blog.objects.order_by('-theme')[:10]
+    output = ', '.join([b.theme for b in latest_blogs_list])
+    return HttpResponse(output)
+
+
+# def (request):
+
 #вместо detail - blog_info
-#create, insert, update
+#create, delete, update
 
 def blog_info(request, blog_id):
     blog = get_object_or_404(Blog, id=blog_id)
-    return render(request, 'blogs/blog.html', {'blog': blog})
+    latest_recordings_list = blog.recording_set.all()
+    output = ', '.join([r.header for r in latest_recordings_list])
+    return HttpResponse(output)
+    #return render(request, 'blogs/blog.html', {'blog': blog})
 
 #def
 
